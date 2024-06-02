6213f269cd695b7bffe67fa4b1f47901c6a40a48
Tao Hansen <taohansens@gmail.com>
2024-06-02 16:45:55 -0300

    add second page

---
+++ frontend/src/pages/About/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Typography } from '@mui/material';
+
+const About: React.FC = () => {
+    return (
+        <div>
+            <Typography variant="h1">About Page</Typography>
+            <Typography>Learn more about us on this page.</Typography>
+        </div>
+    );
+}
+
+export default About;
--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -2,6 +2,7 @@ import {BrowserRouter, Route, Routes} from "react-router-dom";
 import {Container} from "@mui/material";
 
 import Home from "../pages/Home";
+import About from "../pages/About";
 
 const RoutesIndex = () => {
     return (
@@ -9,6 +10,7 @@ const RoutesIndex = () => {
             <Container>
                 <Routes>
                     <Route path="/" element={<Home/>}/>
+                    <Route path="/about" element={<About/>}/>
                 </Routes>
             </Container>
         </BrowserRouter>
